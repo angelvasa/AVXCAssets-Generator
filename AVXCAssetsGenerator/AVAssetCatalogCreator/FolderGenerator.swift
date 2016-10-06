5b6adb492fde207883bd6ceb96d10b8f3f583234
Johnykutty <johnykutty@johny-imac.local>
2016-10-06 10:15:38 +0530

    - Removed .DS_Store - Added .gitignore - Fixed bug creating empty assets for non image files

--- AVXCAssetsGenerator/AVAssetCatalogCreator/FolderGenerator.swift
+++ AVXCAssetsGenerator/AVAssetCatalogCreator/FolderGenerator.swift
@@ -15,46 +15,41 @@ class FolderGenerator: AnyObject {
         let xcAssetPath = destinationLocation + "Assets.xcassets"
         let contentCreator = ContentCreator()
         do {
-           try NSFileManager.defaultManager().createDirectoryAtPath(xcAssetPath, withIntermediateDirectories: true, attributes: nil)
+            try NSFileManager.defaultManager().createDirectoryAtPath(xcAssetPath, withIntermediateDirectories: true, attributes: nil)
             contentCreator.writeDictionaryContent(contentCreator.defaultXcassetDictionary(), atPath: xcAssetPath)
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
+
         for image in folderContent {
             if image == ".DS_Store" { /*ignore that file*/ } else {
-                
+
                 let imageSet = image.removeExtension()
-                
-                
-                
+                let imagePath = origin + "/" + image
+
                 if imageSet == "Icon" || imageSet == "icon" {
+                    guard let isImage = NSImage(contentsOfFile: imagePath) else {
+                        continue
+                    }
                     let appIconPath = xcAssetPath + "/" + "AppIcon.appiconset"
                     do {
                         try NSFileManager.defaultManager().createDirectoryAtPath(appIconPath, withIntermediateDirectories: true, attributes: nil)
-                        
-                        guard let isImage = NSImage(contentsOfFile: origin + image) else {
-                            continue
-                        }
-                        
                         AVIconScalar().scaleImage(isImage, withImageName: imageSet, andSaveAtPath: appIconPath)
-                        
+
                     } catch {
-                        
+
                     }
-                    
-                    
+
+
                 } else {
+                    guard let isImage = NSImage(contentsOfFile: imagePath) else {
+                        continue
+                    }
                     let imageSetPath = xcAssetPath + "/" + imageSet + ".imageset"
                     do {
                         try NSFileManager.defaultManager().createDirectoryAtPath(imageSetPath, withIntermediateDirectories: true, attributes: nil)
-                        
-                        guard let isImage = NSImage(contentsOfFile: origin + image) else {
-                            continue
-                        }
-                        
                         AVImageScalar().scaleImage(isImage, withImageName: imageSet, andSaveAtPath: imageSetPath)
-                        
+
                     } catch {
                         
                     }
